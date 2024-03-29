7a5ee3215f2b086ae3d05dd0e5a62558d7a1768e
ggom121@naver.com <ggom121@naver.com>
2024-03-29 10:21:54 +0900

    ~ 스프링 핵심원리 기본편 섹션 4

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    //여러곳에서 동시에 접근을 하면 문제가 생겨 concurrent해시맵을 쓰는게 맞음
+    private static Map<Long, Member> store = new HashMap<>();
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
