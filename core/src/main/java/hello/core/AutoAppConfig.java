4bbf850fd6b8043977a52a82ddb6c05bd1b357c7
ggom121@naver.com <ggom121@naver.com>
2024-04-06 00:55:47 +0900

    스프링 핵심원리 - 기본편 (조회한 빈이 모두 필요할 때, List, Map)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+//        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-        //Configuration 붙은 클래스는 제외하고 컴포넌트 스캔 사용
+//        //Configuration 붙은 클래스는 제외하고 컴포넌트 스캔 사용
 )
 public class AutoAppConfig {
 
