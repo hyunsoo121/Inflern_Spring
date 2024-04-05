b056f9d1ee2631af4c20a05b5676b95f2e153b98
ggom121@naver.com <ggom121@naver.com>
2024-04-05 21:23:30 +0900

    스프링 핵심원리 - 기본편 (섹션 6: 중복 등록과 충돌)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,9 @@ import org.springframework.context.annotation.FilterType;
         //Configuration 붙은 클래스는 제외하고 컴포넌트 스캔 사용
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -3,6 +3,7 @@ package hello.core.scan.filter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,9 @@ public class ComponentFilterAppConfigTest {
 
     @Test
     void filterScan() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfigTest.class);
-        BeanA beanA= ac.getBean("beanA", BeanA.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
         assertThat(beanA).isNotNull();
-
-        ac.getBean("beanB", BeanB.class);
         assertThrows(
                 NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("beanB", BeanB.class));
